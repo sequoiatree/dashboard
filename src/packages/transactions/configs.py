2b083a57bb0f58536a6fe93aae4f247cc2434284
Sequoia Eyzaguirre <sequoiatree@google.com>
2022-03-11 13:47:42 -0800

    Write docstrings for the configs file.

--- src/packages/transactions/configs.py
+++ src/packages/transactions/configs.py
@@ -1,4 +1,4 @@
-'''...'''
+'''Configs.'''
 
 from typing import *
 
@@ -6,20 +6,13 @@ from . import constants
 
 
 class Configs:
-    '''...'''
+    '''Exposes a JSON configuration object via a user-friendly API.'''
 
     def __init__(
         self,
         configs: constants.JSON,
     ) -> None:
-        '''...
-
-        Args:
-            configs:
-
-        Returns:
-            None.
-        '''
+        '''Initializes the Configs instance.'''
 
         self._configs = configs
 
@@ -27,7 +20,7 @@ class Configs:
         self,
         key: str,
     ) -> constants.JSON:
-        '''...'''
+        '''Gets the requested config.'''
 
         if key not in self._configs:
             raise KeyError()  # TODO: Write a helpful user-friendly message here (missing config). Print the absolute path to the config file too.
